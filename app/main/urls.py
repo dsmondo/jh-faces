3e91a6a69b225e5b3df699ac8cfbb061c970ae6c
JGP <jgp3620@gmail.com>
2024-03-13 13:21:31 +0900

    UPLOADED : Add template file  - main.html

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,7 +1,9 @@
+
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import MainPage
+
 urlpatterns = [
-    path('', include('main.urls')),
-    path('admin/', admin.site.urls),
+    path('', MainPage.as_view()),
 ]
