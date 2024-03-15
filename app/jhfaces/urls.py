0a6130bcec7d7492bc0c6bceb62cf9c47e86c227
JGP <jgp3620@gmail.com>
2024-03-15 09:19:33 +0900

    CHANGED : urls.py (jhfaces)

--- app/jhfaces/urls.py
+++ app/jhfaces/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main.urls')),
+    path('', include('stationInfo.urls')),
+    path('stationInfo/', include('stationInfo.urls')),
     path('admin/', admin.site.urls),
 ]
