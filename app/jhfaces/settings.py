bab01782cdb52a35b93c07f133f6ac07569e9553
JGP <jgp3620@gmail.com>
2024-03-18 15:49:54 +0900

    UPDATED : static files setting (docker-compose.yml)

--- app/jhfaces/settings.py
+++ app/jhfaces/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 # STATIC 파일 설정
 STATIC_URL = '/static/'
-STATIC_ROOT = '/src/static/'
+STATIC_ROOT = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
