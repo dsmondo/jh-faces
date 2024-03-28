93286302974bf027b599b0da034f5b9008782da3
JGP <jgp3620@gmail.com>
2024-03-29 00:53:08 +0900

    UPDATED: - Updated settings.py and urls.py for marketTools configurations.
    수정됨 : marketTools에 관한 설정이 업데이트 되었습니다.
    - settings.py , urls.py :

--- app/jhfaces/urls.py
+++ app/jhfaces/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('stationInfo.urls')),
     path('stationInfo/', include('stationInfo.urls')),
+    path('marketTools/', include('marketTools.urls')),
     path('admin/', admin.site.urls),
 ]
